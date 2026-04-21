aa7eb9427839cc3970c7c49920d033dd94d7413a
Peter <arok276@gmail.com>
2026-04-21 16:56:45 +0800

    更換timeline圖片

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -4,35 +4,35 @@ export const timelineData = [
     region: '馬來西亞 吉隆坡',
     title: '馬來西亞',
     description: '風起雲湧',
-    img: 'https://images.unsplash.com/photo-1596422846543-b5c6488ad993?auto=format&fit=crop&q=80&w=800',
+    img: 'https://media.ct.org.tw/upload/news_article_cms/2026/04/21/77945_64f14b306e7bb.jpg',
   },
   {
     year: '2023',
     region: '印尼 雅加達',
     title: '印尼',
     description: '祂踏浪而來',
-    img: 'https://images.unsplash.com/photo-1518548419970-58e3b4079ab2?auto=format&fit=crop&q=80&w=800',
+    img: 'https://media.ct.org.tw/upload/news_article_cms/2026/04/21/85525_64f1918b31877.jpg',
   },
   {
     year: '2024',
     region: '台灣 高雄',
     title: '台灣',
     description: '往水深之處',
-    img: 'https://images.unsplash.com/photo-1552055944-10ec17565ea7?auto=format&fit=crop&q=80&w=800', // Kaohsiung vibe
+    img: 'https://media.ct.org.tw/upload/news_article_cms/2026/04/21/18181_674d2d6b99f05.jpg', // Kaohsiung vibe
   },
   {
     year: '2025',
     region: '日本 東京',
     title: '日本',
     description: '日出東方',
-    img: 'https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e?auto=format&fit=crop&q=80&w=800', // Japan sunrise vibe
+    img: 'https://media.ct.org.tw/upload/news_article_cms/2026/04/21/39637_海報.jpg', // Japan sunrise vibe
   },
   {
     year: '2026',
     region: '美國 鳳凰城',
     title: '美國',
     description: '曠野中的重生',
-    img: 'https://images.unsplash.com/photo-1505159947321-713197a9f9d7?auto=format&fit=crop&q=80&w=800',
+    img: 'https://media.ct.org.tw/upload/news_article_cms/2026/04/21/30209_632906082_1387636540070401_4268236876262369224_n.jpg',
     active: true,
   }
 ];

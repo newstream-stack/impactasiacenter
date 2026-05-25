025594f00b0a554c9e83ed148a04a5eec7847fc0
Peter <arok276@gmail.com>
2026-05-25 16:36:46 +0800

    小改

--- src/components/AboutIntro/AboutIntro.jsx
+++ src/components/AboutIntro/AboutIntro.jsx
@@ -10,8 +10,8 @@ export default function AboutIntro({ onMoreClick }) {
     <section className={styles.section}>
       <div className={styles.container}>
         <div className={styles.intro}>
-          {about.summary.split('\n').map((line, i) => (
-            <p key={i} className={styles.introLine}>{line}</p>
+          {about.summary.split('\n\n').map((paragraph, i) => (
+            <p key={i} className={styles.introLine}>{paragraph}</p>
           ))}
         </div>
         
